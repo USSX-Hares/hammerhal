b667914c2066a317b4fb89c008a792f575677275
Hares <Hares-Rabbitson@ya.ru>
2017-09-13 10:14:45 +0300

    install.sh added

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo -H apt-get install -y python3
+sudo -H yum install -y python3
+
+sudo -H python3 -m pip install jsonschema
+sudo -H python3 -m pip install Pillow
+
+mkdir logs
+mkdir -p output/heroes
+mkdir -p output/adversaries
