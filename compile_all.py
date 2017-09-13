d09e16243457591d8c23bb529ae1827899f3a436
Hares <Hares-Rabbitson@ya.ru>
2017-09-13 10:20:14 +0300

    temporary commit

---
+++ compile_all.py
@@ -0,0 +1,43 @@
+import json
+import logging
+import logging.config
+import os
+
+from hammerhal.compilers import HeroCompiler
+from hammerhal import ConfigLoader
+
+def setup_logging(
+    default_path='configs/logging.json',
+    default_level=logging.INFO,
+    env_key='LOG_CFG'
+):
+    """Setup logging configuration
+
+    """
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+def start():
+    # Configure logger
+    setup_logging()
+    logger = logging.getLogger('hammerhal')
+    logger.info("Logger started")
+
+    ConfigLoader.load_configs()
+    compiler = HeroCompiler()
+    result = compiler.open('wight-king') and compiler.compile() and compiler.save()
+    if (result):
+        message = "Success! File saved to {filename}".format(filename=result)
+        logger.info(message)
+        print(message)
+
+start()
---
+++ src/hammerhal/__init__.py
@@ -0,0 +1,1 @@
+from hammerhal.config_loader import *
