f83004ec3461852e6859b222d36ae2a5b4f522e2
Peter Zaitcev / USSX.Hares <USSX.Hares@yandex.ru>
2017-09-25 01:05:28 +0300

    Fix: Path to the compilers

--- src/hammerhal/compilers/__init__.py
+++ src/hammerhal/compilers/__init__.py
@@ -1,2 +1,4 @@
 from hammerhal.compilers.compiler_base import *
 from hammerhal.compilers.compiler_module_base import *
+from hammerhal.compilers.hero_compiler import HeroCompiler
+from hammerhal.compilers.adversary_compiler import AdversaryCompiler
