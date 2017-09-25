1ece9bea289de1d66745ab423d8d51755ee3dd5d
Peter Zaitcev / USSX.Hares <USSX.Hares@yandex.ru>
2017-09-25 18:17:48 +0300

    Fix: error in stats module fixed

--- src/hammerhal/compilers/modules/stats_module.py
+++ src/hammerhal/compilers/modules/stats_module.py
@@ -12,7 +12,7 @@ class StatsModule(CompilerModuleBase):
         for stat_name in _stats:
             _x = _stats[stat_name]['x']
             _y = _stats[stat_name]['y']
-            _text_template = "{{{statName}}}{plusSymbol}".format(statName=stat_name, plusSymbol='+' if _stats[stat_name]['x'] else '')
+            _text_template = "{{{statName}}}{plusSymbol}".format(statName=stat_name, plusSymbol='+' if _stats[stat_name]['+'] else '')
             _text = _text_template.format(**self.parent.raw['stats'])
             td.print_line((_x, _y), _text)
 
