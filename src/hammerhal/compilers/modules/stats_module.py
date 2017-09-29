6dda6ce4096373c67f33290d1f2f0d87cb8458ad
Peter Zaitcev / USSX.Hares <USSX.Hares@yandex.ru>
2017-09-29 20:03:16 +0300

    Generator for Stats module added

--- src/hammerhal/compilers/modules/stats_module.py
+++ src/hammerhal/compilers/modules/stats_module.py
@@ -1,5 +1,9 @@
 from hammerhal.compilers.compiler_module_base import CompilerModuleBase
-from hammerhal.text_drawer import TextDrawer
+
+from hammerhal import generator
+if (generator.generator_supported):
+    import System
+    from System import Decimal as decimal
 
 
 class StatsModule(CompilerModuleBase):
@@ -17,3 +21,84 @@ class StatsModule(CompilerModuleBase):
             td.print_line((_x, _y), _text)
 
         self.logger.info("Stats printed")
+
+    ### =======================================
+    ###   WinForms module generator
+    ### =======================================
+
+
+    statsModulePanel = None
+    statsStatLabels = None
+    statsStatValueUpDowns = None
+
+
+    def _create_generator_tab_content(self):
+        _y = 0
+        _tab_index = 0
+
+        self.statsModulePanel = System.Windows.Forms.Panel();
+        self.statsStatLabels = dict()
+        self.statsStatValueUpDowns = dict()
+
+
+        #
+        # statsModulePanel
+        #
+        self.statsModulePanel.Anchor = \
+            System.Windows.Forms.AnchorStyles.Left \
+            | System.Windows.Forms.AnchorStyles.Right \
+            | System.Windows.Forms.AnchorStyles.Top;
+        self.statsModulePanel.Location = System.Drawing.Point(0, 0);
+        self.statsModulePanel.Name = "statsModulePanel";
+        self.statsModulePanel.Size = System.Drawing.Size(222, 429);
+        self.statsModulePanel.TabIndex = 0;
+
+        _stats = self.get_from_module_config('stats')
+        for stat_name in _stats:
+            statsStatLabel = System.Windows.Forms.Label();
+            statsStatValueUpDown = System.Windows.Forms.NumericUpDown();
+
+            #
+            # statsStatLabel
+            #
+            _top = 8
+            statsStatLabel.AutoSize = True;
+            statsStatLabel.Location = System.Drawing.Point(70, _y + _top);
+            statsStatLabel.Name = "stats{stat}Label".format(stat=stat_name.capitalize());
+            statsStatLabel.Size = System.Drawing.Size(35, 13);
+            statsStatLabel.TabIndex = _tab_index;
+            statsStatLabel.Text = "{stat}:".format(stat=stat_name.capitalize());
+            _tab_index += 1
+
+            #
+            # statsStatValueUpDown
+            #
+            _top = 6
+            statsStatValueUpDown.Anchor = \
+                System.Windows.Forms.AnchorStyles.Right \
+                | System.Windows.Forms.AnchorStyles.Top;
+            statsStatValueUpDown.Location = System.Drawing.Point(162, _y + _top);
+            statsStatValueUpDown.Name = "stats{stat}ValueUpDown".format(stat=stat_name.capitalize());
+            statsStatValueUpDown.Size = System.Drawing.Size(54, 20);
+            statsStatValueUpDown.TabIndex = _tab_index;
+            statsStatValueUpDown.Tag = stat_name;
+            statsStatValueUpDown.Value = decimal(self.parent.raw['stats'][stat_name]);
+            statsStatValueUpDown.ValueChanged += System.EventHandler(self.statsStatValueUpDown_ValueChanged);
+            _tab_index += 1
+
+            self.statsModulePanel.Controls.Add(statsStatLabel);
+            self.statsModulePanel.Controls.Add(statsStatValueUpDown);
+            self.statsStatLabels[stat_name] = statsStatLabel
+            self.statsStatValueUpDowns[stat_name] = statsStatValueUpDown
+            _y += statsStatValueUpDown.Height + _top
+
+
+        return self.statsModulePanel;
+
+
+    def statsStatValueUpDown_ValueChanged(self, sender, e):
+        self.setStat(sender.Tag, sender.Value)
+
+    def setStat(self, stat, value):
+        self.parent.raw['stats'][stat] = System.Convert.ToInt32(value)
+        self.update()
