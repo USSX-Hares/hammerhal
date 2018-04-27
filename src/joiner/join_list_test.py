f5ab8ddb287bd4238ff41acaa88e2c7e1b1f2970
Peter Zaitcev / [USSX] Hares <USSX.Hares@yandex.ru>
2018-04-28 01:03:41 +0300

    Joiner Tool: Encoding changed; Tests modifed to use DDT

--- src/joiner/join_list_test.py
+++ src/joiner/join_list_test.py
@@ -1,148 +1,57 @@
 import unittest
 
+from ddt import ddt, data
 from joiner import join_list
-class EmptyListCase(unittest.TestCase):
-    def test_none(self):
-        x = None
+
+@ddt
+class CommonTests(unittest.TestCase):
+
+    def do_test(self, x, estimated_result_1, estimated_result_2):
         result = join_list(x)
         print('"{:}"'.format(result))
-        self.assertEqual(result, "")
+        self.assertEqual(result, estimated_result_1)
         result = join_list(x, ';', ' finally ')
         print('"{:}"'.format(result))
-        self.assertEqual(result, "")
+        self.assertEqual(result, estimated_result_2)
+        
+    @data \
+    (
+        None,
+        list(),
+        set(),
+        dict(),
+        '',
+    )
+    def test_empty_lists(self, input):
+        self.do_test(input, '', '')
 
-    def test_empty_list(self):
-        x = []
-        result = join_list([])
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-        result = join_list([], ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-
-    def test_empty_set(self):
-        x = set()
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-
-    def test_empty_dict(self):
-        x = dict()
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-
-    def test_empty_str(self):
-        x = ''
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "")
-
-class CommonCase(unittest.TestCase):
-    def test_1(self):
-        x = [ "first element" ]
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element")
-
-    def test_2(self):
-        x = [ "first element", "second element" ]
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element, second element")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element finally second element")
-
-    def test_3(self):
-        x = [ "first element", "second element", "third element" ]
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element, second element, third element")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element;second element finally third element")
-
-    def test_4(self):
-        x = [ "first element", "second element", "third element", "fourth element" ]
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element, second element, third element, fourth element")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "first element;second element;third element finally fourth element")
-
-class FormatterCase(unittest.TestCase):
-    def test_simple_formatter(self):
-        x = [ "one", 2, 2.5, "three" ]
-        result = join_list(x)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "one, 2, 2.5, three")
-        result = join_list(x, ';', ' finally ')
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "one;2;2.5 finally three")
-
-    def test_string_simple_formatter(self):
-        x = [ "one", 2, 2.5, "three" ]
-        formatter = "!{0}!"
+    @data \
+    (
+        ([ "first element" ], "first element", "first element"),
+        ([ "first element", "second element" ], "first element, second element", "first element finally second element"),
+        (["first element",  "second element", "third element"], "first element, second element, third element", "first element;second element finally third element"),
+        ([ "first element", "second element", "third element", "fourth element" ], "first element, second element, third element, fourth element", "first element;second element;third element finally fourth element"),
+        ([ "one", 2, 2.5, "three" ], "one, 2, 2.5, three", "one;2;2.5 finally three"),
+    )
+    def test_common_case(self, params):
+        self.do_test(*params)
+    
+    @data \
+    (
+        ([ "one", 2, 2.5, "three" ], "!{0}!", "!one!, !2!, !2.5!, !three!", "!one!;!2!;!2.5! finally !three!"),
+        ([ 16, 2, -42, 621 ], "{:x}", "10, 2, -2a, 26d", "10;2;-2a finally 26d"),
+        ([ 16, 2, -42, 621 ], hex, "0x10, 0x2, -0x2a, 0x26d", "0x10;0x2;-0x2a finally 0x26d"),
+        ([ 16, 2, -42, 621 ], lambda x: oct(abs(x)), "0o20, 0o2, 0o52, 0o1155", "0o20;0o2;0o52 finally 0o1155"),
+        ([ "first element", "second element", "third element", "fourth element" ], lambda x: "**{0}**".format(x.capitalize()), "**First element**, **Second element**, **Third element**, **Fourth element**", "**First element**;**Second element**;**Third element** finally **Fourth element**"),
+    )
+    def test_formatter(self, params):
+        x, formatter, estimated_result_1, estimated_result_2 = params
         result = join_list(x, formatter=formatter)
         print('"{:}"'.format(result))
-        self.assertEqual(result, "!one!, !2!, !2.5!, !three!")
+        self.assertEqual(result, estimated_result_1)
         result = join_list(x, ';', ' finally ', formatter=formatter)
         print('"{:}"'.format(result))
-        self.assertEqual(result, "!one!;!2!;!2.5! finally !three!")
-
-    def test_string_formatter(self):
-        x = [ 16, 2, -42, 621 ]
-        formatter = "{:x}"
-        result = join_list(x, formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "10, 2, -2a, 26d")
-        result = join_list(x, ';', ' finally ', formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "10;2;-2a finally 26d")
-
-    def test_callable_simple_formatter(self):
-        x = [ 16, 2, -42, 621 ]
-        formatter = hex
-        result = join_list(x, formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "0x10, 0x2, -0x2a, 0x26d")
-        result = join_list(x, ';', ' finally ', formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "0x10;0x2;-0x2a finally 0x26d")
-
-    def test_callable_lambda_1_formatter(self):
-        x = [ 16, 2, -42, 621 ]
-        formatter = lambda x: oct(abs(x))
-        result = join_list(x, formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "0o20, 0o2, 0o52, 0o1155")
-        result = join_list(x, ';', ' finally ', formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "0o20;0o2;0o52 finally 0o1155")
-
-    def test_callable_lambda_2_formatter(self):
-        x = [ "first element", "second element", "third element", "fourth element" ]
-        formatter = lambda x: "**{0}**".format(x.capitalize())
-        result = join_list(x, formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "**First element**, **Second element**, **Third element**, **Fourth element**")
-        result = join_list(x, ';', ' finally ', formatter=formatter)
-        print('"{:}"'.format(result))
-        self.assertEqual(result, "**First element**;**Second element**;**Third element** finally **Fourth element**")
+        self.assertEqual(result, estimated_result_2)
 
 class CombatTestingCase(unittest.TestCase):
     def test_card_categories_living_fortress(self):
