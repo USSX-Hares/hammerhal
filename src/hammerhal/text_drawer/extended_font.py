28e1ea096957f8eb5e02cf7d7784bb1191b72179
Hares <Hares-Rabbitson@ya.ru>
2019-02-19 01:14:09 +0300

    Text Drawer: Added new classes

---
+++ src/hammerhal/text_drawer/extended_font.py
@@ -0,0 +1,4 @@
+
+
+class ExtendedFont:
+    pass
---
+++ src/hammerhal/text_drawer/printer.py
@@ -0,0 +1,31 @@
+from typing import List, Tuple
+
+from PIL import ImageFont
+from PIL.ImageDraw import ImageDraw
+from typing_tools import DictStruct
+
+from hammerhal.text_drawer import CapitalizationModes, PrintModes
+from hammerhal.text_drawer.text_funcs import capitalize_first
+
+class Obstacle(DictStruct):
+    x: int
+    y1: int
+    y2: int
+
+class Printer:
+    drawer: ImageDraw
+    
+    _text: str
+    _lines: List[Tuple[str, int]]
+    
+    def print_simple(self, text: str):
+        pass
+    def print_split(self, text: str):
+        pass
+    
+    def _print_text(self, position: Tuple[int, int], c: str):
+        self.drawer.text(position, c, self.color, font=self.font)
+    
+    def _get_text_size(self, c: str):
+        w, h = self.drawer.textsize(c, font=self.font)
+        return w, h
